8d4d9de5ca9bc9a274f3b65d534e58c27a5610a8
Jorge Augusto Rocha de Carvalho <jorge.arcarvalho@tst.jus.br>
2025-04-04 14:53:21 -0300

    Corrige os nomes dos intervalos e ajusta a 5A e a 7d

--- tabuada_do_musico/lib/models/acordes.dart
+++ tabuada_do_musico/lib/models/acordes.dart
@@ -7,20 +7,18 @@ class Acorde {
   final String tipo;
 
   static const Map<String, List<String>> triades = {
-    'Maior': ['1J', '3M', '5J'],
-    'menor': ['1J', '3m', '5J'],
-    'Aum': ['1J', '3M', '6m'], // 6m pra facilitar (implementar 5A)
-    'dim': ['1J', '3m', '5d'],
+    '': ['1J', '3M', '5J'],
+    'm': ['1J', '3m', '5J'],
+    '5+': ['1J', '3M', '5A'],
     'sus4': ['1J', '4J', '5J']
   };
 
   static const Map<String, List<String>> tetrades = {
-    '7M': ['1J', '3M', '5J', '7M'], // maior com 7M: 1, 3M, 5J, 7M
-    '7': ['1J', '3M', '5J', '7m'], // dominante: 1, 3M, 5J, 7m
-    'min7': ['1J', '3m', '5J', '7m'], // menor com 7m: 1, 3m, 5J, 7m
-    'm7b5': ['1J', '3m', '5d', '7m'], // meio diminuto: 1, 3m, b5, 7m
-    'dim': ['1J', '3m', '5d', '6M'], // diminuto: 1, 3m, b5, 7d (ou 7bb) (6M)
-    // Implementar 7d
+    '7M': ['1J', '3M', '5J', '7M'],
+    '7': ['1J', '3M', '5J', '7m'],
+    'm7': ['1J', '3m', '5J', '7m'],
+    'm7b5': ['1J', '3m', '5d', '7m'],
+    '°': ['1J', '3m', '5d', '7d'],
   };
 
   static Map<String, List<String>> get formulas => {

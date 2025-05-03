a3eb1e0dd57ecdac9073ce05296cec695d044d7d
povaloid <rc.jorgeaugusto@gmail.com>
2025-05-03 17:19:06 -0300

    [EXIN-3] Retira import inutilizado

--- tabuada_do_musico/lib/pages/menu/modulo1/intervalos/intervalos_livre_page.dart
+++ tabuada_do_musico/lib/pages/menu/modulo1/intervalos/intervalos_livre_page.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:tcc_app/database/tonalidades/bemois.dart';
 import 'package:tcc_app/database/tonalidades/sustenidos.dart';
